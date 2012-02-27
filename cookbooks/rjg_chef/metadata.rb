9972c2a0a1348ffae4018ad164777911ac668f3f
rgeyer <me@ryangeyer.com>
2012-02-27 17:53:17 +0000

    Initial commit of rjg_chef cookbook

---
+++ cookbooks/rjg_chef/metadata.rb
@@ -0,0 +1,8 @@
+maintainer       "Ryan J. Geyer"
+maintainer_email "ryan.geyer@rightscale.com"
+license          "All rights reserved"
+description      "Installs/Configures rjg_chef"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
+version          "0.0.1"
+
+recipe "rjg_chef::default","Prints a message to the output"
---
+++ cookbooks/rjg_chef/recipes/default.rb
@@ -0,0 +1,10 @@
+#
+# Cookbook Name:: rjg_chef
+# Recipe:: default
+#
+# Copyright 2012, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+log "Hello there to the CTE dev team"
